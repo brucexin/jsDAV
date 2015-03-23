8b434b4ffb2ee9f3ed62b72ab40450c117846a60
bruce <qixin@QideMacBook-Pro.local>
2015-03-23 23:41:34 +0800

    add display name and favorite flag on node

--- examples/fileserver.js
+++ examples/fileserver.js
@@ -10,10 +10,17 @@
 var jsDAV = require("./../lib/jsdav");
 jsDAV.debugMode = true;
 var jsDAV_Locks_Backend_FS = require("./../lib/DAV/plugins/locks/fs");
+var jsDAV_FS_Directory = require("./../lib/DAV/backends/fs/directory");
+
+function make_desktop() {
+  var desktop = jsDAV_FS_Directory.new("c:/Users/qixin");
+  desktop.setDisplayName("DESKTOP");
+  return desktop;
+}
 
 jsDAV.createServer({
     //node: __dirname + "/../test/assets",
     //locksBackend: jsDAV_Locks_Backend_FS.new(__dirname + "/../test/assets")
-    node: "c:/",
+    node: [jsDAV_FS_Directory.new("c:/"), make_desktop()],
     locksBackend: jsDAV_Locks_Backend_FS.new("c:/")
 }, 8000, "10.211.55.3");
